8cf5d64e28f98b0f0642f175c1f042d8225f013e
Linus <fearthedear@users.noreply.github.com>
2016-11-18 17:34:42 +0800

    Update rot2stay_script.sh
    updated for better detection and sleep mode after apartments detected

--- rot2stay_script.sh
+++ rot2stay_script.sh
@@ -7,11 +7,24 @@ do
 
     if [ "$count" != "0" ]
     then
-    	mail -s "New apartments on rotti2stay yeah" linuskinzel@gmail.com < mailtext.txt
+    	mail -s "New apartments on rot2stay" linuskinzel@gmail.com alexander.holzmann.ekholm@gmail.com paul.r.tesar@gmail.com newstuffexp@gmail.com < mailtext.txt
     	echo "mail sent"
-    	sleep 86400
+    	sleep 30
+    	apartments_online=true
+    	while [[ $apartments_online == true ]]
+    		do
+    		count1=0
+    		count1=`curl -s "https://rotterdam2stay.nl/book-apartment.html" | grep -c "filter"`
+    		if [ "$count1" == "0" ]
+    		then
+    			apartments_online=false
+    		else
+    			sleep 30
+    		fi
+    	done
+
     else
     	echo "no new apartments. no mail send"
-    	sleep 600
+    	sleep 60
     fi
 done
